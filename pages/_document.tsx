91df973d3599d4e1f59f584bd1a29e6f354170ba
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-05-09 14:11:54 -0300

    Begin of the project

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.gstatic.com"
+                        crossOrigin="true"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;600;700&family=Lato:ital,wght@0,400;0,700;1,400&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
